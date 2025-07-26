a4ce05c772a0a5630ce4b24bc0d450b5e1c3639c
Pojemnik <andrzej.gauza@outlook.com>
2025-07-26 18:52:50 +0200

    Add namespace, update Core'Nest

--- Runtime/DataWaste/DataWasteWorker.cs
+++ Runtime/DataWaste/DataWasteWorker.cs
@@ -7,189 +7,192 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
-internal class DataWasteWorker
+namespace StorkStudios.DataWaste
 {
-    private readonly HttpClient httpClient;
-
-    private readonly Thread workerThread;
-    private readonly BlockingCollection<MessageWrapper> messageQueue = new BlockingCollection<MessageWrapper>();
-    private readonly string playerId;
-    private readonly string gameId;
-    private readonly TaskCompletionSource<object> flushTask = new TaskCompletionSource<object>();
-
-    private class GetDataMessage : IData
+    internal class DataWasteWorker
     {
-        private string path;
-        public object Data => path;
+        private readonly HttpClient httpClient;
 
-        public GetDataMessage(string path)
+        private readonly Thread workerThread;
+        private readonly BlockingCollection<MessageWrapper> messageQueue = new BlockingCollection<MessageWrapper>();
+        private readonly string playerId;
+        private readonly string gameId;
+        private readonly TaskCompletionSource<object> flushTask = new TaskCompletionSource<object>();
+
+        private class GetDataMessage : IData
         {
-            this.path = path;
+            private string path;
+            public object Data => path;
+
+            public GetDataMessage(string path)
+            {
+                this.path = path;
+            }
         }
-    }
 
-    public DataWasteWorker(Uri uri, string playerId, string gameId)
-    {
-        this.playerId = playerId;
-        this.gameId = gameId;
-        httpClient = new HttpClient()
+        public DataWasteWorker(Uri uri, string playerId, string gameId)
         {
-            BaseAddress = uri
-        };
+            this.playerId = playerId;
+            this.gameId = gameId;
+            httpClient = new HttpClient()
+            {
+                BaseAddress = uri
+            };
 
-        workerThread = new Thread(WorkerLoop);
-        workerThread.Start();
-    }
+            workerThread = new Thread(WorkerLoop);
+            workerThread.Start();
+        }
 
-    public Task<string> GetServerStatus()
-    {
-        TaskCompletionSource<string> completionSource = new TaskCompletionSource<string>();
-        messageQueue.Add(new MessageWrapper(MessageWrapper.MessageType.StatusCheck, completionSource));
-        return completionSource.Task;
-    }
+        public Task<string> GetServerStatus()
+        {
+            TaskCompletionSource<string> completionSource = new TaskCompletionSource<string>();
+            messageQueue.Add(new MessageWrapper(MessageWrapper.MessageType.StatusCheck, completionSource));
+            return completionSource.Task;
+        }
 
-    public void SendData(IData data)
-    {
-        messageQueue.Add(new MessageWrapper(MessageWrapper.MessageType.SendData, data));
-    }
+        public void SendData(IData data)
+        {
+            messageQueue.Add(new MessageWrapper(MessageWrapper.MessageType.SendData, data));
+        }
 
-    public Task<string> GetData(string path)
-    {
-        TaskCompletionSource<string> completionSource = new TaskCompletionSource<string>();
-        messageQueue.Add(new MessageWrapper(MessageWrapper.MessageType.GetData, new GetDataMessage(path), completionSource));
-        return completionSource.Task;
-    }
+        public Task<string> GetData(string path)
+        {
+            TaskCompletionSource<string> completionSource = new TaskCompletionSource<string>();
+            messageQueue.Add(new MessageWrapper(MessageWrapper.MessageType.GetData, new GetDataMessage(path), completionSource));
+            return completionSource.Task;
+        }
 
-    public Task FlushAndFinish()
-    {
-        messageQueue.CompleteAdding();
-        return flushTask.Task;
-    }
+        public Task FlushAndFinish()
+        {
+            messageQueue.CompleteAdding();
+            return flushTask.Task;
+        }
 
-    private void WorkerLoop()
-    {
-        while (!messageQueue.IsCompleted)
+        private void WorkerLoop()
+        {
+            while (!messageQueue.IsCompleted)
+            {
+                try
+                {
+                    MessageWrapper message = messageQueue.Take();
+                    HandleMessage(message);
+                }
+                catch (InvalidOperationException)
+                {
+                    //Ignore exception caused by Take() on queue completed in the meantime
+                }
+            }
+            flushTask.SetResult(null);
+        }
+
+        private async void HandleMessage(MessageWrapper message)
+        {
+            switch (message.Type)
+            {
+                case MessageWrapper.MessageType.SendData:
+                    {
+                        await SendMessage(message);
+                    }
+                    break;
+                case MessageWrapper.MessageType.StatusCheck:
+                    {
+                        await SendStatusCheck(message);
+                    }
+                    break;
+                case MessageWrapper.MessageType.GetData:
+                    {
+                        await GetDataFromServer(message);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async Task SendStatusCheck(MessageWrapper messageWrapper)
         {
             try
             {
-                MessageWrapper message = messageQueue.Take();
-                HandleMessage(message);
-            }
-            catch (InvalidOperationException)
-            {
-                //Ignore exception caused by Take() on queue completed in the meantime
-            }
-        }
-        flushTask.SetResult(null);
-    }
-
-    private async void HandleMessage(MessageWrapper message)
-    {
-        switch (message.Type)
-        {
-            case MessageWrapper.MessageType.SendData:
+                HttpResponseMessage response = await httpClient.GetAsync("status");
+                if (response.IsSuccessStatusCode)
                 {
-                    await SendMessage(message);
+                    string content = await response.Content.ReadAsStringAsync();
+                    messageWrapper.Task.SetResult(content);
+                    return;
                 }
-                break;
-            case MessageWrapper.MessageType.StatusCheck:
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                messageWrapper.Task.SetResult("");
+            }
+        }
+
+        private async Task SendMessage(MessageWrapper messageWrapper)
+        {
+            try
+            {
+                HttpContent content = CreateTelemetryMessageBody(messageWrapper);
+                HttpResponseMessage response = await httpClient.PostAsync($"telemetry/{gameId}", content);
+
+                if (messageWrapper.Task == null)
                 {
-                    await SendStatusCheck(message);
+                    return;
                 }
-                break;
-            case MessageWrapper.MessageType.GetData:
+
+                if (response.IsSuccessStatusCode)
                 {
-                    await GetDataFromServer(message);
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    messageWrapper.Task.SetResult(responseString);
+                    return;
                 }
-                break;
-            default:
-                break;
-        }
-    }
-
-    private async Task SendStatusCheck(MessageWrapper messageWrapper)
-    {
-        try
-        {
-            HttpResponseMessage response = await httpClient.GetAsync("status");
-            if (response.IsSuccessStatusCode)
+            }
+            catch (Exception)
             {
-                string content = await response.Content.ReadAsStringAsync();
-                messageWrapper.Task.SetResult(content);
-                return;
+
+            }
+            finally
+            {
+                messageWrapper.Task.SetResult("");
             }
         }
-        catch (Exception)
-        {
-        }
-        finally
-        {
-            messageWrapper.Task.SetResult("");
-        }
-    }
 
-    private async Task SendMessage(MessageWrapper messageWrapper)
-    {
-        try
+        private async Task GetDataFromServer(MessageWrapper messageWrapper)
         {
-            HttpContent content = CreateTelemetryMessageBody(messageWrapper);
-            HttpResponseMessage response = await httpClient.PostAsync($"telemetry/{gameId}", content);
-
-            if (messageWrapper.Task == null)
+            try
             {
-                return;
+                HttpResponseMessage response = await httpClient.GetAsync($"extras/{gameId}/{messageWrapper.Content.Data}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    messageWrapper.Task.SetResult(responseString);
+                    return;
+                }
             }
-
-            if (response.IsSuccessStatusCode)
+            catch (Exception)
             {
-                string responseString = await response.Content.ReadAsStringAsync();
-                messageWrapper.Task.SetResult(responseString);
-                return;
+
+            }
+            finally
+            {
+                messageWrapper.Task.SetResult("");
             }
         }
-        catch (Exception)
-        {
 
-        }
-        finally
+        private HttpContent CreateTelemetryMessageBody(MessageWrapper messageWrapper)
         {
-            messageWrapper.Task.SetResult("");
-        }
-    }
-
-    private async Task GetDataFromServer(MessageWrapper messageWrapper)
-    {
-        try
-        {
-            HttpResponseMessage response = await httpClient.GetAsync($"extras/{gameId}/{messageWrapper.Content.Data}");
-
-            if (response.IsSuccessStatusCode)
-            {
-                string responseString = await response.Content.ReadAsStringAsync();
-                messageWrapper.Task.SetResult(responseString);
-                return;
-            }
-        }
-        catch (Exception)
-        {
-
-        }
-        finally
-        {
-            messageWrapper.Task.SetResult("");
-        }
-    }
-
-    private HttpContent CreateTelemetryMessageBody(MessageWrapper messageWrapper)
-    {
-        Dictionary<string, object> data = new Dictionary<string, object>
+            Dictionary<string, object> data = new Dictionary<string, object>
         {
             { "playerId", playerId },
             { "timestamp", messageWrapper.Timestamp },
             { "data", messageWrapper.Content.Data }
         };
-        HttpContent content = new StringContent(JsonConvert.SerializeObject(data));
-        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-        return content;
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return content;
+        }
     }
 }
--- Runtime/Telemetry/Telemetry.cs
+++ Runtime/Telemetry/Telemetry.cs
@@ -5,193 +5,197 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using UnityEngine;
+using StorkStudios.CoreNest;
 
-public class Telemetry : Singleton<Telemetry>
+namespace StorkStudios.DataWaste
 {
-    private enum ServerStatus
+    public class Telemetry : Singleton<Telemetry>
     {
-        Unknown,
-        Online,
-        Offline
-    }
-
-    [Header("Config")]
-    [SerializeField]
-    [Tooltip("This should be disabled during development, to limit load on the telemetry server")]
-    private bool enableTelemetry;
-    [SerializeField]
-    private string telemetryServerAddress;
-    [SerializeField]
-    private string gameId;
-
-    [Header("Debug")]
-    [SerializeField]
-    private bool debugInfo;
-    [SerializeField]
-    private bool sendInitPackage = true;
-
-    [SerializeField]
-    [ReadOnly]
-    private ServerStatus serverStatus = ServerStatus.Unknown;
-
-    protected override void Awake()
-    {
-        if (!enableTelemetry)
+        private enum ServerStatus
         {
-            return;
+            Unknown,
+            Online,
+            Offline
         }
 
-        string playerId = GetPlayerId();
+        [Header("Config")]
+        [SerializeField]
+        [Tooltip("This should be disabled during development, to limit load on the telemetry server")]
+        private bool enableTelemetry;
+        [SerializeField]
+        private string telemetryServerAddress;
+        [SerializeField]
+        private string gameId;
+
+        [Header("Debug")]
+        [SerializeField]
+        private bool debugInfo;
+        [SerializeField]
+        private bool sendInitPackage = true;
+
+        [SerializeField]
+        [ReadOnly]
+        private ServerStatus serverStatus = ServerStatus.Unknown;
+
+        protected override void Awake()
+        {
+            if (!enableTelemetry)
+            {
+                return;
+            }
+
+            string playerId = GetPlayerId();
 #if UNITY_EDITOR
-        Debug.LogWarning("Telemetry is enabled - it should be only enabled in production builds!");
-        playerId = "editor";
+            Debug.LogWarning("Telemetry is enabled - it should be only enabled in production builds!");
+            playerId = "editor";
 #else
         Debug.Log($"Telemetry running. PlayerId: {playerId}");
 #endif
-        DataWaste.InitInstance(new Uri(telemetryServerAddress), telemetryServerAddress, gameId);
+            DataWaste.InitInstance(new Uri(telemetryServerAddress), telemetryServerAddress, gameId);
 
-        Init();
+            Init();
 
-        base.Awake();
-    }
-
-    private void Start()
-    {
-        if (!enableTelemetry || !sendInitPackage)
-        {
-            return;
-        }
-        SendApplicationStartMessage();
-        SendSystemInfoMessage();
-    }
-
-    private void OnApplicationQuit()
-    {
-        if (!enableTelemetry || serverStatus == ServerStatus.Offline)
-        {
-            return;
+            base.Awake();
         }
 
-        SendTelemetryMessage(new TelemetryMessage("applicationQuit"));
-        DataWaste.Instance.FlushAndFinish();
-    }
-
-    public void SendTelemetryMessage(TelemetryMessage message)
-    {
-        //In a short window after startup we are sending messages to the server with unknown status
-        //If server is running it will get the message, if not we will handle the error in DataWaste
-        //When server status is determined as offline, we stop
-        if (!enableTelemetry || serverStatus == ServerStatus.Offline)
+        private void Start()
         {
-            return;
-        }
-        DataWaste.Instance.SendData(message);
-    }
-
-    public Task<string> GetServerStatus()
-    {
-        return DataWaste.Instance.GetServerStatus();
-    }
-
-    public void GetData(string path, Action<string> callback)
-    {
-        Task<string> task = DataWaste.Instance.GetData(path);
-        task.ContinueWith(t =>
-        {
-            callback(t.Result);
-        });
-    }
-
-    private void Init()
-    {
-        Task<string> task = DataWaste.Instance.GetServerStatus();
-        task.ContinueWith(t =>
-        {
-            if (t.Result == "OK")
+            if (!enableTelemetry || !sendInitPackage)
             {
-                serverStatus = ServerStatus.Online;
-                if (debugInfo)
+                return;
+            }
+            SendApplicationStartMessage();
+            SendSystemInfoMessage();
+        }
+
+        private void OnApplicationQuit()
+        {
+            if (!enableTelemetry || serverStatus == ServerStatus.Offline)
+            {
+                return;
+            }
+
+            SendTelemetryMessage(new TelemetryMessage("applicationQuit"));
+            DataWaste.Instance.FlushAndFinish();
+        }
+
+        public void SendTelemetryMessage(TelemetryMessage message)
+        {
+            //In a short window after startup we are sending messages to the server with unknown status
+            //If server is running it will get the message, if not we will handle the error in DataWaste
+            //When server status is determined as offline, we stop
+            if (!enableTelemetry || serverStatus == ServerStatus.Offline)
+            {
+                return;
+            }
+            DataWaste.Instance.SendData(message);
+        }
+
+        public Task<string> GetServerStatus()
+        {
+            return DataWaste.Instance.GetServerStatus();
+        }
+
+        public void GetData(string path, Action<string> callback)
+        {
+            Task<string> task = DataWaste.Instance.GetData(path);
+            task.ContinueWith(t =>
+            {
+                callback(t.Result);
+            });
+        }
+
+        private void Init()
+        {
+            Task<string> task = DataWaste.Instance.GetServerStatus();
+            task.ContinueWith(t =>
+            {
+                if (t.Result == "OK")
+                {
+                    serverStatus = ServerStatus.Online;
+                    if (debugInfo)
+                    {
+                        Debug.Log("Telemetry server is running");
+                    }
+
+                    CheckNewGameVersion();
+                }
+                else
+                {
+                    serverStatus = ServerStatus.Offline;
+                    if (debugInfo)
+                    {
+                        Debug.LogWarning("Telemetry server is unavailable");
+                    }
+                }
+            });
+        }
+
+        private void CheckNewGameVersion()
+        {
+            Task<string> task = DataWaste.Instance.GetNewestGameVersion();
+            task.ContinueWith(t =>
+            {
+                GameVersionData gameVersion = JsonConvert.DeserializeObject<GameVersionData>(t.Result);
+                if (gameVersion.VersionIndex > GameVersion.Instance.VersionIndex)
+                {
+                    Debug.Log($"New game version is available - {gameVersion.VersionName}");
+                    GameVersion.Instance.NewestAvailableVersion = gameVersion;
+                }
+            });
+        }
+
+        private void SendApplicationStartMessage()
+        {
+            TelemetryMessage message = new TelemetryMessage("applicationStart");
+            message.AddProperty("version", GameVersion.Instance.VersionText);
+            SendTelemetryMessage(message);
+        }
+
+        private void SendSystemInfoMessage()
+        {
+            TelemetryMessage message = new TelemetryMessage("systemInfo");
+            Type systemInfoType = typeof(SystemInfo);
+            foreach (PropertyInfo info in systemInfoType.GetProperties(BindingFlags.Public | BindingFlags.Static))
+            {
+                message.AddProperty(info.Name, info.GetValue(systemInfoType, null).ToString());
+            }
+            SendTelemetryMessage(message);
+        }
+
+        private string GetPlayerId()
+        {
+            if (PlayerPrefs.HasKey("playerId"))
+            {
+                return PlayerPrefs.GetString("playerId");
+            }
+            else
+            {
+                string id = Guid.NewGuid().ToString();
+                PlayerPrefs.SetString("playerId", id);
+                return id;
+            }
+        }
+
+        [InvokeButton("Test server")]
+        private void TestServerStatus()
+        {
+            if (DataWaste.Instance == null)
+            {
+                DataWaste.InitInstance(new Uri(telemetryServerAddress), telemetryServerAddress, gameId);
+            }
+            Task<string> task = DataWaste.Instance.GetServerStatus();
+            task.ContinueWith(t =>
+            {
+                if (t.Result == "OK")
                 {
                     Debug.Log("Telemetry server is running");
                 }
-
-                CheckNewGameVersion();
-            }
-            else
-            {
-                serverStatus = ServerStatus.Offline;
-                if (debugInfo)
+                else
                 {
                     Debug.LogWarning("Telemetry server is unavailable");
                 }
-            }
-        });
-    }
-
-    private void CheckNewGameVersion()
-    {
-        Task<string> task = DataWaste.Instance.GetNewestGameVersion();
-        task.ContinueWith(t =>
-        {
-            GameVersionData gameVersion = JsonConvert.DeserializeObject<GameVersionData>(t.Result);
-            if (gameVersion.VersionIndex > GameVersion.Instance.VersionIndex)
-            {
-                Debug.Log($"New game version is available - {gameVersion.VersionName}");
-                GameVersion.Instance.NewestAvailableVersion = gameVersion;
-            }
-        });
-    }
-
-    private void SendApplicationStartMessage()
-    {
-        TelemetryMessage message = new TelemetryMessage("applicationStart");
-        message.AddProperty("version", GameVersion.Instance.VersionText);
-        SendTelemetryMessage(message);
-    }
-
-    private void SendSystemInfoMessage()
-    {
-        TelemetryMessage message = new TelemetryMessage("systemInfo");
-        Type systemInfoType = typeof(SystemInfo);
-        foreach (PropertyInfo info in systemInfoType.GetProperties(BindingFlags.Public | BindingFlags.Static))
-        {
-            message.AddProperty(info.Name, info.GetValue(systemInfoType, null).ToString());
+            });
         }
-        SendTelemetryMessage(message);
-    }
-
-    private string GetPlayerId()
-    {
-        if (PlayerPrefs.HasKey("playerId"))
-        {
-            return PlayerPrefs.GetString("playerId");
-        }
-        else
-        {
-            string id = Guid.NewGuid().ToString();
-            PlayerPrefs.SetString("playerId", id);
-            return id;
-        }
-    }
-
-    [InvokeButton("Test server")]
-    private void TestServerStatus()
-    {
-        if (DataWaste.Instance == null)
-        {
-            DataWaste.InitInstance(new Uri(telemetryServerAddress), telemetryServerAddress, gameId);
-        }
-        Task<string> task = DataWaste.Instance.GetServerStatus();
-        task.ContinueWith(t =>
-        {
-            if (t.Result == "OK")
-            {
-                Debug.Log("Telemetry server is running");
-            }
-            else
-            {
-                Debug.LogWarning("Telemetry server is unavailable");
-            }
-        });
     }
 }
