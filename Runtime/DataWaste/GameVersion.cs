d6e991fc00fd4cb77e5d2c9d960290d8c08a5942
Pojemnik <andrzej.gauza@outlook.com>
2026-02-28 19:14:14 +0100

    Cleanup game version script

--- Runtime/DataWaste/GameVersion.cs
+++ Runtime/DataWaste/GameVersion.cs
@@ -6,9 +6,14 @@ using StorkStudios.CoreNest;
 
 namespace StorkStudios.DataWaste
 {
-    [CreateAssetMenu(fileName = "GameVersion", menuName = "StorkStudios/DataWaste/Game version")]
-    public class GameVersionIndex : ScriptableObjectSingleton<GameVersion>
+    [CreateAssetMenu(fileName = "GameVersion", menuName = "Singletons/Game version")]
+    public class GameVersion : ScriptableObjectSingleton<GameVersion>
     {
+        [SerializeField]
+        [Tooltip("Version name that is diplayed in game")]
+        private string versionText;
+        public string VersionText => versionText;
+
         [SerializeField]
         [Tooltip("Version number that should be incremented with every release. It is used to compare versions")]
         private int versionIndex;
